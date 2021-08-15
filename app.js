dfbb1f9128d0af9e94f8c4135db6ba892ef94b89
Shivani Narang <shivaninarang20@gmail.com>
2021-08-15 12:41:26 +0100

    bonus added

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ const outputTxt=document.querySelector('#output-txt');
 
 function calculateProfitAndLoss(initial,quantity,current)
 {
-    
+    initial=Number(initial);
+    quantity=Number(quantity);
+    current=Number(current);
+    if( !isNaN(initial) && !isNaN(quantity) && !isNaN(current))
+    {
     if(initial>current)
     {
-        const loss=(initial - current)*quantity;
-        const lossPercentage=(loss/initial)*100;
+        const loss=((initial - current)*quantity).toFixed(2);
+        const lossPercentage=((loss/initial)*100).toFixed(2);
        
         outputTxt.innerText="Hey the loss is "+ loss +" and the percentage is "+ lossPercentage +"%.😭";
         outputTxt.style.color='red';
@@ -19,8 +23,8 @@ function calculateProfitAndLoss(initial,quantity,current)
     }
     else if(current>initial)
     {
-        const profit=(current-initial)*quantity;
-        const profitPercentage=(profit/initial)*100;
+        const profit=((current-initial)*quantity).toFixed(2);
+        const profitPercentage=((profit/initial)*100).toFixed(2);
         outputTxt.innerText="Yay the profit is "+ profit +" and the percentage is "+ profitPercentage +"%.😍";
         outputTxt.style.color='green';
         outputTxt.toFixed(2);
@@ -31,14 +35,20 @@ function calculateProfitAndLoss(initial,quantity,current)
 
     }
 }
+else
+{
+    outputTxt.innerText="Please enter all the Fields";
+}
+
+}
 
 
 function submitHandler()
 {
     
-    const ini=Number(initialPrice.value);
-    const quant=Number(stocks.value);
-    const curr=Number(currentPrice.value);
+    const ini=initialPrice.value;
+    const quant=stocks.value;
+    const curr=currentPrice.value;
   
     
     calculateProfitAndLoss(ini,quant,curr);
